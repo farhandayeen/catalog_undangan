5d2f61677a7cb7656c2a0cfc2d0e0f3e7fda35a9
farhandayeen <farhandayeen@gmail.com>
2026-03-15 04:12:05 +0800

    update catalog.tsx

--- components/landing/catalog.tsx
+++ components/landing/catalog.tsx
@@ -135,7 +135,7 @@ export function Catalog() {
         </div>
 
         {/* Invitation Grid */}
-        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8">
+        <div className="grid grid-cols-2 gap-3">
           {filteredInvitations.map((invitation) => (
             <Card 
               key={invitation.id} 
